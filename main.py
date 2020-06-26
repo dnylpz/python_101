ae5dc9d0abc470f7d7501f70b3fa87722ff1d5e7
Daniel Lopez <danielespinoza_lopez@apple.com>
2020-06-25 23:07:07 -0700

    completa clase 2
    Signed-off-by: Daniel Lopez <danielespinoza_lopez@apple.com>

--- main.py
+++ main.py
@@ -1,67 +1,75 @@
-#coding=UTF8
-### Tipos de datos basicos
+# Digamos que quieres hacer una lista de productos con precios y descripcion.
+# lo podrias hacer asi
+bicicleta_precio = 100.0
+bicicleta_descripcion = "una bicicleta bonita"
 
-numero_entero = 12 #numero entero sin signo decimal
-print(numero_entero)
-# ademas del sistema decimal existen sistemas numericos diferentes dependiendo de la base numerica que se utilice 
-# o el numero de digitos para pasar al siguiente numero ej Decimal (0-9) binario (0-1) Octal (0-8) Hexadecimal (0-f) 
-numero_entero_en_binario =  0b1100 # 12 en binario 
-print(numero_entero_en_binario)
-numero_entero_en_octal = 0o14
-print(numero_entero_en_octal)
-numero_entero_en_hexadecimal = 0xc
-print(numero_entero_en_hexadecimal)
+# ahora quieres un producto diferente
+pelota_precio = 10.0
+pelota_descripcion = "una pelota redonda"
 
-print(numero_entero_en_hexadecimal == numero_entero)
+# evidentemente si necesitas mantener 1000 productos esto no es muy buena idea que tal que pudieramos guardar
+# algunas cosas relacionadas en una sola variable?, para eso existen las colecciones o estructuras de datos, la mas basica es la lista
 
-# numeros con punto decimal (flotante)
-numero_con_punto = 3.141592653589793
-numero_con_exponente = 314e-2
-numero_con_exponente_2 = 314e1
-print(numero_con_exponente)
+lista_de_precios = [100.0, 10.0]
+lista_de_descripciones = ['una bicicleta bonita', 'una pelota redonda']
 
-# numeros complejos (Exclusivos de python)
-numero_imaginario_o_complejo = 2+3j # la j indica un numero que conocemos, se usa para representar funciones
+#si queremos acceder los datos de la bicicleta  solo tenemos que accederlos en el mismo INDICE
+print("bicicleta cuesta ${} y es: {}".format(lista_de_precios[0],lista_de_descripciones[0])) #explicaremos format mas adelante
+print("pelota cuesta ${} y es: {}".format(lista_de_precios[1],lista_de_descripciones[1]))
 
-# Caracteres (letras)
-letra = 'a'
-print(letra)
-print(ord(letra)) #letras tambien son numeros
+# en otros lenguajes de programacion los listas no pueden tener diferentes tipos de datos pero en python es posible
 
-# caracteres especiales
-letra = u"\U0001F604"
-print(letra.encode('utf8'))
-print(repr(letra))
-
-# Cadenas (muchos caracteres seguidos)
-cadena = "hola!"
-# las cadenas no solo son palabras solas
-palabras = "Hola Mundo!"
-
-#las cadenas pueden incluir caracteres especiales como comillas o diagonales
-cadena_con_especiales = "Hey You \n\"by Pink Floyd\""
-print(cadena_con_especiales)
-
-cadenas_crudas = r'Hey You \n\"by Pink Floyd\"' #cadenas que no 'escapan' los char especiales
-
-print(cadenas_crudas)
+bicicleta = [100.00, 'una bicicleta bonita']
+print('bicicleta cuesta ${} y es: {}'.format(bicicleta[0],bicicleta[1])) 
 
 
-# Cadenas con muchas lineas
-parrafo = """
-Lorem ipsum dolor sit amet, 
-consectetur adipiscing elit. In nec felis in diam varius luctus. 
-Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam auctor vitae velit sed rhoncus. 
-Sed diam sapien, auctor a lacus in, ornare venenatis metus. Nam vestibulum nisi id libero rhoncus luctus. 
-Sed a placerat mi. Curabitur quis nisi purus. Nullam vulputate ligula dui, quis pretium eros suscipit id. 
-Cras est velit, vehicula sit amet nibh at, blandit pulvinar ex. Suspendisse potenti.
-"""
-print(parrafo)
+# side note, las cadenas tambien son como listas!, puedes acceder a sus miembros con sus indices
+print('hola mundo'[0:4])
 
 
+#tambien puede haber listas de listas!
+pelota = [10.00, 'una pelota redonda']
+productos = [bicicleta,pelota]
+print(productos[1])
 
-# Booleanos 
-verdad = True
-mentira = False 
+# puedes unir dos listas 
+juguetes = [[50.00,'un muneco'],[60.00,'un carrito']]
+productos = productos + juguetes
+print(productos)
 
-print(verdad == mentira)
+# en python existe otro tipo de lista, inmutable (es decir no se pueden cambiar) la tupla, la verda
+# hasta ahora no recuerdo el uso de las tuplas, pero estaba chido
+
+categorias = ('juguetes','articulos deportivos')
+print(categorias)
+# categorias[0] = 'comida'  # esto manda un error, intenta ver el error quitando el # al inicio de la linea
+
+# tambien existen los sets, que son como listas pero no pueden repetir sus elementos
+un_set = {'hola','mundo','hola'}
+print(un_set)
+#no tienen orden por lo que no puedes acceder por un indice
+#print(un_set[0]) # esto manda un error
+# les puedes agregar mas elementos con la funcion add
+un_set.add('grande')
+print(un_set)
+
+# pero regresemos a los productos, aun con los arreglos de productos, es impractico recordar TODOS los indices, para eso existen los diccionarios
+
+productos = {
+  'bici': {
+    'precio': 100.00,
+    'descripcion': 'Una bicicleta bonita'
+  },
+  'pelota': {
+    'precio': 10.00,
+    'descripcion': 'una pelota redonda'
+  } 
+}
+
+print(productos)
+print(productos['bici']['precio'])
+
+# regresando a las cadenas, como los arreglos tienen varias funciones para saber atributos sobre ellos despues veremos mas sobre ellos
+print(len('hola mundo!!!'))
+print(len(productos))
+print(len(juguetes))
